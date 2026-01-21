1d1a5451ee047b54b9fc50d3971bc3a40d9e266a
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-21 09:46:53 +0100

    feat(core): implement core router layer

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,48 +1,137 @@
 <?php
+
 namespace App\Core;
 
 class Router
 {
-    // Gestion des routes
-    public function dispatch() {}
-}
+    /**
+     * array for storage the url
+     */
+    private static array $routes = [];
+    /**
+    * This is a core part of the Singleton pattern to ensure only one
+    */ 
+    private static  $router;
 
-class Controller
-{
-    // Controller parent
-}
+    private function __construct() {}
 
-class Model
-{
-    // Model parent
-}
+    /***
+     * for make sure there is a connection.
+     */
+    public static function getRouter(): router
+    {
+        if (!isset(self::$router)) {
+            self::$router = new self();
+        }
+        return self::$router;
+    }
 
-class View
-{
-    // Rendu Twig
-}
+    private function register(string $route, string $method, array|callable $action)
+    {
+        $route = trim($route, '/');
 
-class Database
-{
-    // Connexion PostgreSQL (PDO)
-}
+        /**
+         * Assign action to the passed route
+         */
+        self::$routes[$method][$route] = $action;
+    }
 
-class Auth
-{
-    // Authentification & rôles
-}
+    public function get(string $route, array|callable $action)
+    {
+        $this->register($route, 'GET', $action);
+    }
 
-class Validator
-{
-    // Validation des données
-}
+    public function post(string $route, array|callable $action)
+    {
+        $this->register($route, 'POST', $action);
+    }
 
-class Security
-{
-    // CSRF, XSS, protections
-}
+    public function put(string $route, array|callable $action)
+    {
+        $this->register($route, 'PUT', $action);
+    }
 
-class Session
-{
-    // Gestion des sessions
+    public function delete(string $route, array|callable $action)
+    {
+        $this->register($route, 'DELETE', $action);
+    }
+
+    /**
+     *  Resolve the current request to the corresponding route action.
+     */
+
+    public function dispatch()
+    {
+        $requestRoute = trim(
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+            '/'
+        );
+        $routes = self::$routes[$_SERVER['REQUEST_METHOD']];
+
+        foreach ($routes as $route => $action) {
+            $routeRegex = preg_replace_callback('/{\w+(:([^}]+))?}/', function ($matches) {
+                return isset($matches[2]) ? '(' . $matches[2] . ')' : '([a-zA-Z0-9_-]+)';
+            }, $route);
+
+            /**
+             * Add the start and end delimiters.
+             */
+            $routeRegex = '@^' . $routeRegex . '$@';
+
+            /**
+             * Check if the requested route matches the current route pattern.
+             */
+            if (preg_match($routeRegex, $requestRoute, $matches)) {
+                array_shift($matches);
+
+                /**
+                 * Get all user requested path params values after removing the first matches.  
+                 */
+                $routeParamsValues = $matches;
+
+                /**
+                 * Find all route params names from route and save in $routeParamsNames
+                 */
+                $routeParamsNames = [];
+                if (preg_match_all('/{(\w+)(:[^}]+)?}/', $route, $matches)) {
+                    $routeParamsNames = $matches[1];
+                }
+
+                /**
+                 *  Combine between route parameter names and user provided parameter values.
+                 */
+                $routeParams = array_combine($routeParamsNames, $routeParamsValues);
+
+                return $this->resolveAction($action, $routeParams);
+            }
+        }
+        return $this->abort('404 Page not found');
+    }
+
+    private function resolveAction($action, $routeParams)
+    {
+        if (is_callable($action)) {
+            return call_user_func_array($action, $routeParams);
+        } else if (is_array($action)) {
+            $controllerClass = $action[0];
+            $method = $action[1];
+
+            if (!class_exists($controllerClass) || !is_subclass_of($controllerClass, 'App\\core\\Controller')) {
+                throw new \Exception('Invalid controller class');
+            }
+
+            $controller = new $controllerClass();
+
+            if (!method_exists($controller, $method)) {
+                throw new \Exception('Method not found');
+            }
+
+            return call_user_func_array([$controller, $method], $routeParams);
+        }
+    }
+    private function abort(string $message, int $code = 404)
+    {
+        http_response_code($code);
+        throw new \Exception($message, $code);
+    }
 }
